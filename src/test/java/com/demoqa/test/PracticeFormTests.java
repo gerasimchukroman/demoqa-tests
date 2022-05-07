34130e671c85d399f9cdce5e56886578ad0bde40
79270 <roman.gerasimchuk@simbirsoft.com>
2022-05-07 21:11:34 +0400

    middle save

--- src/test/java/com/demoqa/test/PracticeFormTests.java
+++ src/test/java/com/demoqa/test/PracticeFormTests.java
@@ -2,6 +2,8 @@ package com.demoqa.test;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class PracticeFormTests {
     @Test
     void fillFormTest(){
