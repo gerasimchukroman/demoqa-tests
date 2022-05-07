ed1271ed3d13a7812767468be66414e88766ebe0
79270 <roman.gerasimchuk@simbirsoft.com>
2022-05-07 21:27:59 +0400

    end day

--- src/test/java/com/demoqa/test/PracticeFormTests.java
+++ src/test/java/com/demoqa/test/PracticeFormTests.java
@@ -1,12 +1,43 @@
 package com.demoqa.test;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeFormTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.startMaximized = true;
+    }
     @Test
     void fillFormTest(){
+
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String userEmail = "ab@ab.com";
+        String userNumber = "8999999999";
+        String currentAddress = "Pushkina street, Kolotuchkina home";
+
+
         open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail );
+
+        //Gender
+
+        $("#userNumber").setValue(userNumber);
+
+        //Date of Birth
+        //Subjects
+        //Hobbies
+        //Picture
+
+        $("#currentAddress").setValue(currentAddress);
+
+        //State and city
     }
 }
