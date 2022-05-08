c03ea8e54aeac0061d3c846afc2f091c1369c06f
79270 <roman.gerasimchuk@simbirsoft.com>
2022-05-08 19:29:03 +0400

    middle save

--- src/test/java/com/demoqa/test/PracticeFormTests.java
+++ src/test/java/com/demoqa/test/PracticeFormTests.java
@@ -4,8 +4,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class PracticeFormTests {
     @BeforeAll
@@ -22,16 +24,22 @@ public class PracticeFormTests {
         String currentAddress = "Pushkina street, Kolotuchkina home";
 
 
+        String value = "Female";
+
+
         open("https://demoqa.com/automation-practice-form");
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail );
-
-        //Gender
-
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue(userNumber);
 
         //Date of Birth
+       $(".react-datepicker__month-select").selectOption("November");
+       $(".react-datepicker__year-select").selectOption("1994");
+       $(".react-datepicker__day--001:not(.react-datepicker__day--outside-month)").click();
+
+
         //Subjects
         //Hobbies
         //Picture
